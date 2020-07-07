f51a8dafb2fafab2fd85dcd107a369ebf7d2e8e6
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-07 14:01:32 +0200

    Updates

---
+++ precache-manifest.a3cc1e1a2cb821a3c7adbda9fba622af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23069716e242839a4a2badcb345f26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4f5eb73c61ac6d0a0951",
+    "url": "/static/css/main.da079710.chunk.css"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/static/js/2.a1b12462.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.a1b12462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5eb73c61ac6d0a0951",
+    "url": "/static/js/main.f90cbc43.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f7303d665c1c61a6b3f1079575e40848",
+    "url": "/static/media/contact-keeper.f7303d66.png"
+  },
+  {
+    "revision": "71f35d476a8943d2f3837b597e20fd83",
+    "url": "/static/media/github-finder.71f35d47.png"
+  },
+  {
+    "revision": "9dab37dae31a25fb6611824dce3aabb2",
+    "url": "/static/media/it-logger.9dab37da.png"
+  },
+  {
+    "revision": "199d57e022ee33aba57c560f001876e3",
+    "url": "/static/media/profile.199d57e0.jpeg"
+  }
+]);
