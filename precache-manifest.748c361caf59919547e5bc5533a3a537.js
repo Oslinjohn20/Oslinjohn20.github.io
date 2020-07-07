e9ead900a0d33537d49a32fdc7cbd291b7461b93
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-07 14:14:52 +0200

    Updates

---
+++ precache-manifest.748c361caf59919547e5bc5533a3a537.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a4c33131375e35717d1eece2333699",
+    "url": "/Oslinjohn20.github.io/index.html"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/Oslinjohn20.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1c2ce063fae6f963764a",
+    "url": "/Oslinjohn20.github.io/static/css/main.da079710.chunk.css"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/Oslinjohn20.github.io/static/js/2.a1b12462.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Oslinjohn20.github.io/static/js/2.a1b12462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2ce063fae6f963764a",
+    "url": "/Oslinjohn20.github.io/static/js/main.5ae7a334.chunk.js"
+  },
+  {
+    "revision": "c1bef351c54b63bb4ca2",
+    "url": "/Oslinjohn20.github.io/static/js/runtime-main.4c76d094.js"
+  },
+  {
+    "revision": "f7303d665c1c61a6b3f1079575e40848",
+    "url": "/Oslinjohn20.github.io/static/media/contact-keeper.f7303d66.png"
+  },
+  {
+    "revision": "71f35d476a8943d2f3837b597e20fd83",
+    "url": "/Oslinjohn20.github.io/static/media/github-finder.71f35d47.png"
+  },
+  {
+    "revision": "9dab37dae31a25fb6611824dce3aabb2",
+    "url": "/Oslinjohn20.github.io/static/media/it-logger.9dab37da.png"
+  },
+  {
+    "revision": "199d57e022ee33aba57c560f001876e3",
+    "url": "/Oslinjohn20.github.io/static/media/profile.199d57e0.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3cc1e1a2cb821a3c7adbda9fba622af.js"
+  "/Oslinjohn20.github.io/precache-manifest.748c361caf59919547e5bc5533a3a537.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Oslinjohn20.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
